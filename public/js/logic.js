748a97dbaf74dd1f090da44e67d7172663d94e55
hemasali <hemo3li7@gmail.com>
2018-07-24 15:35:14 +0300

    Create Dom file & logic

---
+++ public/js/logic.js
@@ -0,0 +1,26 @@
+// function fetch (url,cb){
+// var xhr = new XMLHttpRequest();
+// xhr.onreadystatechange=function(){
+//     if(xhr.readyState==4){
+//         if(xhr.status==200){
+
+//             var fileText = JSON.parse(xhr.responseText)
+//             cb(fileText)
+//         }
+//         else{
+          
+//         }
+//     }
+// }
+
+// xhr.open("GET",url,true);
+// xhr.send();
+
+// }
+function getElementById(element){
+    return document.getElementById(element);
+}
+
+function createElement(elemnt){
+    return document.createElement(elemnt);
+}
