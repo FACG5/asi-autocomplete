de75418ea35c4d6a7d7cdc8d6769dbaca87b183a
hemasali <hemo3li7@gmail.com>
2018-07-24 17:06:04 +0300

    adding fetch function

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,22 +1,22 @@
-// function fetch (url,cb){
-// var xhr = new XMLHttpRequest();
-// xhr.onreadystatechange=function(){
-//     if(xhr.readyState==4){
-//         if(xhr.status==200){
+function fetch (url,cb){
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange=function(){
+    if(xhr.readyState==4){
+        if(xhr.status==200){
 
-//             var fileText = JSON.parse(xhr.responseText)
-//             cb(fileText)
-//         }
-//         else{
+            var fileText = JSON.parse(xhr.responseText)
+            cb(fileText)
+        }
+        else{
           
-//         }
-//     }
-// }
+        }
+    }
+}
 
-// xhr.open("GET",url,true);
-// xhr.send();
+xhr.open("GET",url,true);
+xhr.send();
 
-// }
+}
 function getElementById(element){
     return document.getElementById(element);
 }
