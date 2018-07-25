8da544e21d3750771be5b8dd00a3dfa7ec332a8d
hemasali <hemo3li7@gmail.com>
2018-07-25 11:19:06 +0300

    get data from api

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-var{serverStaticFile,handelhomepage,autocomplete,handelError}=require("./functions.js")
+var{serverStaticFile,handelhomepage,autocomplete,handelError,writeFile}=require("./functions.js")
 function router(req, res) {
   var endpoint = req.url;
   if (endpoint === "/") {
@@ -6,7 +6,13 @@ function router(req, res) {
 }
   else if (endpoint==="/autocomplete"){
   autocomplete(req,res);
-  }else {
+
+  
+}
+else if (endpoint==="/writeData"){
+  writeFile(req,res);
+}
+else {
     serverStaticFile(req, res);
   }
 }
