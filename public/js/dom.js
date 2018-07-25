65d3ee5913c2b5048b3b791c0031b828f6fe9bbf
hemasali <hemo3li7@gmail.com>
2018-07-25 12:37:28 +0300

    adding the model in index and dom to show the information

--- public/js/dom.js
+++ public/js/dom.js
@@ -8,87 +8,81 @@ for (let i = 1; i <= 10; i++) {
     for (var j = 0; j < result.Extracts.length; j++) {
       totalResult.push(result.Extracts[j].author);
     }
-      counter++;
-      if(counter ==10){
-        
-        
-    sendData("/writeData", JSON.stringify(totalResult), function() {
-      fetch("/autocomplete", function(fileText) {
-        var arr = fileText;
-        var input = getElementById("word");
+    counter++;
+    if (counter == 10) {
+      sendData("/writeData", JSON.stringify(totalResult), function() {
+        fetch("/autocomplete", function(fileText) {
+          var arr = fileText;
+          var input = getElementById("word");
 
-        input.addEventListener("input", function(e) {
-          removeDiv("result");
+          input.addEventListener("input", function(e) {
+            removeDiv("result");
+            var result = createElement("div");
+            result.setAttribute("class", "result");
+            result.setAttribute("id", "result");
+            var mainContainer = getElementById("MainContainerr");
+            mainContainer.appendChild(result);
 
-          var result = createElement("div");
-          result.setAttribute("class", "result");
-          result.setAttribute("id", "result");
-          var mainContainer = getElementById("MainContainerr");
-          mainContainer.appendChild(result);
-
-          var word = this.value;
-          if (!word) removeDiv("result");
-          for (let i = 0; i < arr.length; i++) {
             var word = this.value;
-            if (
-              arr[i].substr(0, word.length).toUpperCase() == word.toUpperCase()
-            ) {
-              // removeDiv("result")
-              var resultElemnt = createElement("div");
-              resultElemnt.setAttribute("class", "item");
-              // resultElemnt.setAttribute("class","resultElemnt");
-              resultElemnt.textContent = arr[i];
-              var inputElement = createElement("input");
-              inputElement.setAttribute("type", "hidden");
-              inputElement.value = arr[i];
-              resultElemnt.appendChild(inputElement);
+            if (!word) removeDiv("result");
+            for (let i = 0; i < arr.length; i++) {
+              var word = this.value;
+              if (
+                arr[i].substr(0, word.length).toUpperCase() ==
+                word.toUpperCase()
+              ) {
+                // removeDiv("result")
+                var resultElemnt = createElement("div");
+                resultElemnt.setAttribute("class", "item");
+                // resultElemnt.setAttribute("class","resultElemnt");
+                resultElemnt.textContent = arr[i];
+                var inputElement = createElement("input");
+                inputElement.setAttribute("type", "hidden");
+                inputElement.value = arr[i];
+                resultElemnt.appendChild(inputElement);
 
-              resultElemnt.addEventListener("click", function(e) {
-                input.value = this.getElementsByTagName("input")[0].value; //return to above resultElemnt
+                resultElemnt.addEventListener("click", function(e) {
+                  input.value = this.getElementsByTagName("input")[0].value; //return to above resultElemnt
 
-                removeDiv("result");
-              });
-              result.appendChild(resultElemnt);
+                  removeDiv("result");
+                });
+                result.appendChild(resultElemnt);
+              }
             }
-          }
-        });
+          });
 
-        var submit = getElementById("submit");
+          var submit = getElementById("submit");
+          var info = createElement("div");
+          info.setAttribute("id", "info");
+          var modalContent = getElementById("modal-content");
+          submit.addEventListener("click", function(e) {
+            e.preventDefault();
 
-        submit.addEventListener("click", function(e) {
-          e.preventDefault();
+            modalContent.appendChild(info);
+            var name = input.value;
+            if (name) {
+              var url =
+                "http://extracts.panmacmillan.com/getextracts?authorcontains=" +
+                name;
+              fetch(url, function(result) {
+                if (result.PageCount != 0) {
+                  var authorName = result.Extracts[0].author;
+                  var authorBiography = result.Extracts[0].authorBiography;
+                  clearDivAndSet("info",authorBiography)
+                  showModel();
+                } else {
+                  clearDivAndSet("info","null")
 
-          var name = input.value;
-          var url =
-            "http://extracts.panmacmillan.com/getextracts?authorcontains=" +
-            name;
-          fetch(url, function(result) {
-            var authorName = result.Extracts[0].author;
-            var authorBiography = result.Extracts[0].authorBiography;
-            console.log(0);
-            console.log(authorName);
-            console.log(authorBiography);
-            var modal = document.getElementById('myModal');
-            var span = document.getElementsByClassName("close")[0];
-
-    modal.style.display = "block";
-    window.onclick = function(event) {
-      if (event.target == modal) {
-          modal.style.display = "none";
-      }
-      span.onclick = function() {
-        modal.style.display = "none";
-    }
-    
-  }
-
-
+                  showModel();
+                }
+              });
+            } else {
+              clearDivAndSet("info","null")
+              showModel();
+            }
           });
         });
       });
-    });
-      }
-    
-
+    }
   });
 }
--- public/js/logic.js
+++ public/js/logic.js
@@ -7,7 +7,6 @@ function fetch(url, cb) {
         cb(fileText);
       } else {
         console.log("Xx");
-        
       }
     }
   };
@@ -16,20 +15,20 @@ function fetch(url, cb) {
   xhr.send();
 }
 
-function sendData(url,data,cb) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState == 4) {
-        if (xhr.status == 200) {
-          cb();
-        } else {
-        }
+function sendData(url, data, cb) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      if (xhr.status == 200) {
+        cb();
+      } else {
       }
-    };
-  
-    xhr.open("POST", url, true);
-    xhr.send(JSON.stringify(data));
-  }
+    }
+  };
+
+  xhr.open("POST", url, true);
+  xhr.send(JSON.stringify(data));
+}
 function getElementById(element) {
   return document.getElementById(element);
 }
@@ -41,3 +40,23 @@ function removeDiv(name) {
   var divTodelete = document.getElementById(name);
   if (divTodelete) divTodelete.parentNode.removeChild(divTodelete);
 }
+function showModel() {
+  var modal = document.getElementById("myModal");
+  var span = document.getElementsByClassName("close")[0];
+
+  modal.style.display = "block";
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+  span.onclick = function() {
+    modal.style.display = "none";
+  };
+}
+
+function clearDivAndSet(name,Text) {
+  var div = getElementById(name);
+  div.innerHTML = Text;
+}
+
